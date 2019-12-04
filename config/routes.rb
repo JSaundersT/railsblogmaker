25f477af32bfc7e6934ff3922778b06590b3db1c
James Saunders <jtsgithub@gmail.com>
2019-12-04 08:24:28 +0000

    Reset app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
 
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+
+  #These are changed
+
+end
